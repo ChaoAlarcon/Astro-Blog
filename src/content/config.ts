5d01fa5bf441795916d6601a17a020e98731fb56
ChaoAlarcón <chaoalarcon03@gmail.com>
2025-04-06 11:24:06 +0200

    Visualización de posts

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,17 +1,29 @@
-import{z, defineCollection}from 'astro:content'
+import { z, defineCollection, reference } from 'astro:content';
 
 const blogCollection = defineCollection({
-    type:'content',
+    type: 'content',
     schema: z.object({
         title: z.string(),
         description: z.string(),
         publishedAt: z.date(),
-        draft: z.boolean().optional(),
+        draft: z.boolean(),
         tags: z.array(z.string()),
-        category: z.string().optional(),
+        category: z.string(),
+        author: reference('authors')
+    })
+});
+
+const authorsCollection = defineCollection({
+    type: 'data',
+    schema: z.object({
+        name: z.string(),
+        email: z.string(),
+        linkedin: z.string(),
+        image: z.string()
     })
 })
 
 export const collections = {
-    blog: blogCollection,
+    'blog': blogCollection,
+    'authors': authorsCollection
 }
